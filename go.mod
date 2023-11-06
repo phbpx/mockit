2ca7a1636df97bb54d0f24c18dba478163ab0759
Paulo Bortolotti <bortolotti.paulo@gmail.com>
2023-11-06 15:18:35 -0300

    feat: add template support

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/phbpx/mockit
 go 1.21.3
 
 require (
+	github.com/brianvoe/gofakeit/v6 v6.24.0
 	github.com/dimfeld/httptreemux v5.0.1+incompatible
 	gopkg.in/yaml.v3 v3.0.1
 )
--- mockit.go
+++ mockit.go
@@ -1,10 +1,19 @@
 package mockit
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
 	"net/http"
+	"regexp"
+	"time"
 
+	"github.com/brianvoe/gofakeit/v6"
 	"github.com/dimfeld/httptreemux"
 )
+
+// regex to match {{ param }}
+var paramRegex = regexp.MustCompile(`{{.+}}`)
 
 // Response represents a mock response.
 type Response struct {
@@ -29,17 +38,70 @@ type Config struct {
 func NewRouter(config Config) (http.Handler, error) {
 	router := httptreemux.New()
 
-	for _, endpoint := range config.Endpoints {
-		// register endpoint
-		router.Handle(endpoint.Method, endpoint.URL, func(w http.ResponseWriter, r *http.Request, m map[string]string) {
-			w.WriteHeader(endpoint.Response.Code)
+	for _, e := range config.Endpoints {
+		endpoint := e
+
+		router.Handle(endpoint.Method, endpoint.URL, func(w http.ResponseWriter, r *http.Request, params map[string]string) {
+			body, err := compile(endpoint.Response.Body, params)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
 			for k, v := range endpoint.Response.Headers {
 				w.Header().Set(k, v)
 			}
 
-			w.Write([]byte(endpoint.Response.Body))
+			w.WriteHeader(endpoint.Response.Code)
+			if _, err := w.Write(body); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 		})
 	}
 
 	return router, nil
+}
+
+func compile(text string, params map[string]string) ([]byte, error) {
+	if !paramRegex.MatchString(text) {
+		return []byte(text), nil
+	}
+
+	funcMap := template.FuncMap{
+		"params":    paramsValue(params),
+		"uuid":      gofakeit.UUID,
+		"now":       time.Now,
+		"username":  gofakeit.Username,
+		"name":      gofakeit.Name,
+		"email":     gofakeit.Email,
+		"phone":     gofakeit.Phone,
+		"url":       gofakeit.URL,
+		"userAgent": gofakeit.UserAgent,
+		"ipv4":      gofakeit.IPv4Address,
+		"ipv6":      gofakeit.IPv6Address,
+		"int":       gofakeit.Int64,
+		"digit":     gofakeit.Digit,
+		"digitN":    gofakeit.DigitN,
+		"letter":    gofakeit.Letter,
+		"letterN":   gofakeit.LetterN,
+	}
+
+	t, err := template.New("").Funcs(funcMap).Parse(text)
+	if err != nil {
+		return nil, fmt.Errorf("parse template: %w", err)
+	}
+
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, nil); err != nil {
+		return nil, fmt.Errorf("execute template: %w", err)
+	}
+
+	return tpl.Bytes(), nil
+}
+
+func paramsValue(params map[string]string) func(string) string {
+	return func(key string) string {
+		return params[key]
+	}
 }
